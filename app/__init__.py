e1547058c1a55c0c66767a62e4d3cd9210b87d03
Loping151 <wangkailing151@gmail.com>
2025-09-24 15:54:11 +0800

    0.3.4

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@
 BotShepherd应用核心模块
 """
 
-__version__ = "0.3.3" # 准备快进到 1.0.0
+__version__ = "0.3.4" # 准备快进到 1.0.0
 __author__ = "Loping151"
 __github__ = "https://github.com/Loping151/BotShepherd"
 __description__ = "星星花与牧羊人 - 多框架统一管理平台"
